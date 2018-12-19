eac381c927be5f3a418adf56f1ed715f61d1459e
Michael Hardy <98.michael.hardy@gmail.com>
2018-12-18 22:57:49 -0800

    Switching to mini UPNP because there are more miniupnp devices in the US

--- soapserver/src/main.rs
+++ soapserver/src/main.rs
@@ -2,7 +2,7 @@
 
 #[macro_use] extern crate rocket;
 
-#[get("/description.xml")]
+#[get("/rootDesc.xml")]
 fn index() -> &'static str {
 	include_str!("description.xml")
 }
@@ -13,7 +13,7 @@ fn index() -> &'static str {
  * DATE: Tue, 11 Dec 2007 09:13:18 GMT
  * LENGTH: 1057
  * MODIFIED: Tue, 11 Dec 2007 09:13:18 GMT
- * SERVER: Linux/2.6.5-it0, UPnP/1.0, Intel SDK for UPnP devices /1.2
+ * SERVER: Linux/2.6, UPnP/1.0, miniupnpd/1.0
  */
 fn main() {
 	rocket::ignite()
